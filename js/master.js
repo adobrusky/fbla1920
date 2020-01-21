47bc085e6aaf6ff6997850cd593f5c0f4490b91d
thefierytortoise <austindobrusky@gmail.com>
2020-01-21 15:36:24 -0600

    work on animatiopn

--- js/master.js
+++ js/master.js
@@ -1,12 +1,18 @@
 let scroll;
-//use screenWidth, large, and medium from ProjectA for retreiving window width and breakpoints
 
+function slideRight() {
+
+}
+
+//use screenWidth, large, and medium from ProjectA for retreiving window width and breakpoints
 function changeTheme() {
   if(screenWidth > large) {
     if(scroll >= $('.nav').height()) {
       $('.nav').css('backgroundColor', '#11172b');
+      $('.nav-items').css('backgroundColor', 'transparent');
     } else {
       $('.nav').css('backgroundColor', 'rgba(17,23,43,0.56)');
+      $('.nav-items').css('backgroundColor', 'transparent');
     }
   } else if(screenWidth > medium && screenWidth < large) {
     if(scroll >= ($('.nav').height() + $('.nav-items').height())) {
@@ -14,10 +20,11 @@ function changeTheme() {
       $('.nav-items').css('backgroundColor', '#11172b');
     } else {
       $('.nav').css('backgroundColor', 'rgba(17,23,43,0.56)');
-      $('.nav-items').css('backgroundColor', 'transparent');
+      $('.nav-items').css('backgroundColor', 'rgba(17,23,43,0.56)');
     }
   } else {
     $('.nav').css('backgroundColor', '#11172b');
+    $('.nav-items').css('backgroundColor', '#11172b');
   }
 };
 
