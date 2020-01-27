f4efd97317ce61f6d5d7861a5a68650db0c2e28a
thefierytortoise <austindobrusky@gmail.com>
2020-01-27 15:36:34 -0600

    continue working on flight booking system

--- js/schedule.js
+++ js/schedule.js
@@ -1,7 +1,15 @@
 function hiddenSelection() {
-  
+    $('.hiddenSelect').each(function() {
+      $(this).siblings().children('span').html($(this).find('.active').html());
+    })
+};
+
+let passengers = 1;
+function passengerSelection() {
+  $('.passengerSelect').siblings().children('span').html(passengers);
 };
 
 $(document).ready(function() {
   hiddenSelection();
+  passengerSelection();
 });
