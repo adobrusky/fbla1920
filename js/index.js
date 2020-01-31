d7e6f0c4a290ca82ad989f496da0a9521695a73c
tortul <austindobrusky@gmail.com>
2020-01-30 21:03:25 -0600

    finish fixing the bug with dropdown transitions

--- js/index.js
+++ js/index.js
@@ -4,23 +4,23 @@ let scroll;
 function changeTheme() {
   if(screenWidth > large) {
     if(scroll >= $('.nav').height()) {
-      $('.nav').css('backgroundColor', '#11172b');
+      $('.nav').css('backgroundColor', '#0e1221');
       $('.nav-items').css('backgroundColor', 'transparent');
     } else {
-      $('.nav').css('backgroundColor', 'rgba(17,23,43,0.56)');
+      $('.nav').css('backgroundColor', 'rgba(14,18,33,0.56)');
       $('.nav-items').css('backgroundColor', 'transparent');
     }
   } else if(screenWidth > medium && screenWidth < large) {
     if(scroll >= ($('.nav').height() + $('.nav-items').height())) {
-      $('.nav').css('backgroundColor', '#11172b');
-      $('.nav-items').css('backgroundColor', '#11172b');
+      $('.nav').css('backgroundColor', '#0e1221');
+      $('.nav-items').css('backgroundColor', '#0e1221');
     } else {
-      $('.nav').css('backgroundColor', 'rgba(17,23,43,0.56)');
-      $('.nav-items').css('backgroundColor', 'rgba(17,23,43,0.56)');
+      $('.nav').css('backgroundColor', 'rgba(14,18,33,0.56)');
+      $('.nav-items').css('backgroundColor', 'rgba(14,18,33,0.56)');
     }
   } else {
-    $('.nav').css('backgroundColor', '#11172b');
-    $('.nav-items').css('backgroundColor', '#11172b');
+    $('.nav').css('backgroundColor', '#0e1221');
+    $('.nav-items').css('backgroundColor', '#0e1221');
   }
 };
 
--- js/schedule.js
+++ js/schedule.js
@@ -1,25 +1,56 @@
 function hiddenSelection() {
-    $('.hiddenSelect').each(function() {
-      $(this).siblings().children('span').html($(this).find('.active').html());
-    })
-
-    $('.flightInfo p').click(function() {
-      if(!$(this).closest('div').hasClass('activeDrop')) {
-        $(this).closest('div').siblings('div').removeClass('activeDrop');
-        $(this).closest('div').addClass('activeDrop');
-      } else {
-        $(this).closest('div').removeClass('activeDrop');
-      }
-    });
+  $('.hiddenSelect').each(function() {
+    $(this).siblings().children('span').html($(this).find('.active').html());
+  })
 };
+
+$('.flightInfo p').click(function() {
+  if(!$(this).closest('div').hasClass('activeDrop')) {
+    $(this).closest('div').siblings('div').removeClass('activeDrop');
+    $(this).closest('div').addClass('activeDrop');
+  } else {
+    $(this).closest('div').removeClass('activeDrop');
+  }
+});
+
+$('.hiddenSelect li').each(function() {
+  $(this).click(function() {
+    if(!$(this).hasClass('active')) {
+      $(this).siblings('.active').removeClass('active');
+      $(this).addClass('active');
+      $(this).closest('.hiddenSelect').siblings().children('span').fadeOut(300);
+      setTimeout(function () {
+        hiddenSelection();
+        hiddenSelectionWidth();
+      }, 300);
+      $(this).closest('.hiddenSelect').siblings().children('span').fadeIn(300);
+    }
+  });
+});
 
 function hiddenSelectionWidth() {
   $('.flightInfo ul').each(function() {
-    $(this).css('width', $(this).siblings('p').outerWidth());
+    let test = $(this).siblings('p').outerWidth();
+    $(this).animate({width : test });
+    // finish fixing this bug
   });
 }
 
 let passengers = 1;
+$('i.fa.fa-plus').click(function() {
+  if(passengers < 9) {
+    passengers++;
+    passengerSelection();
+  }
+});
+
+$('i.fa.fa-minus').click(function() {
+  if(passengers > 1) {
+    passengers--;
+    passengerSelection();
+  }
+});
+
 function passengerSelection() {
   $('.passengerSelect').siblings().children('span').html(passengers);
 };
